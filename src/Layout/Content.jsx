47aa60991ef271b613bca283bfee4d53c66e5919
Bhaumic Singh <bhaumiksingh2000@gmail.com>
2023-10-09 12:50:38 +0530

    device box changes
    Signed-off-by: Bhaumic Singh <bhaumiksingh2000@gmail.com>

--- src/Layout/Content.jsx
+++ src/Layout/Content.jsx
@@ -1,4 +1,4 @@
-import ItemBox from '../components/ItemBox';
+import DeviceItemBox, { LabBox, ProjectItemBox } from '../components/ItemBox';
 import Banner from '../components/Banner';
 import { FolderOpenIcon } from '@heroicons/react/24/outline';
 import { BeakerIcon } from '@heroicons/react/24/outline';
@@ -63,24 +63,54 @@ const Home = () => {
       >
         Build Beyond Reality
       </div>
-      {menuItems.map((item, index) => (
-        <div key={index}>
-          <div
-            id={item.name}
-            className='flex items-center gap-x-8 my-6'
-          >
-            {item.icon}
-            <h1 className='text-gray-200 text-lg text-start font-bold tracking-tight md:text-xl lg:text-3xl'>
-              {item.name}
-            </h1>
-            <hr className='w-full' />
-            <button className='p-1 border-2 rounded bg-twinpurple-300'>
-              <PlusMarkSvg size={`h-4 w-4`} />
-            </button>
-          </div>
-          <ItemBox />
+      <div>
+        <div
+          id={menuItems[0].name}
+          className='flex items-center gap-x-8 my-6'
+        >
+          {menuItems[0].icon}
+          <h1 className='text-gray-200 text-lg text-start font-bold tracking-tight md:text-xl lg:text-3xl'>
+            {menuItems[0].name}
+          </h1>
+          <hr className='w-full' />
+          <button className='p-1 border-2 rounded bg-twinpurple-300'>
+            <PlusMarkSvg size={`h-4 w-4`} />
+          </button>
         </div>
-      ))}
+        <DeviceItemBox />
+      </div>
+      <div>
+        <div
+          id={menuItems[1].name}
+          className='flex items-center gap-x-8 my-6'
+        >
+          {menuItems[1].icon}
+          <h1 className='text-gray-200 text-lg text-start font-bold tracking-tight md:text-xl lg:text-3xl'>
+            {menuItems[1].name}
+          </h1>
+          <hr className='w-full' />
+          <button className='p-1 border-2 rounded bg-twinpurple-300'>
+            <PlusMarkSvg size={`h-4 w-4`} />
+          </button>
+        </div>
+        <ProjectItemBox />
+      </div>
+      <div>
+        <div
+          id={menuItems[2].name}
+          className='flex items-center gap-x-8 my-6'
+        >
+          {menuItems[2].icon}
+          <h1 className='text-gray-200 text-lg text-start font-bold tracking-tight md:text-xl lg:text-3xl'>
+            {menuItems[2].name}
+          </h1>
+          <hr className='w-full' />
+          <button className='p-1 border-2 rounded bg-twinpurple-300'>
+            <PlusMarkSvg size={`h-4 w-4`} />
+          </button>
+        </div>
+        <LabBox />
+      </div>
     </>
   );
 };
--- src/components/ItemBox.jsx
+++ src/components/ItemBox.jsx
@@ -1,50 +1,63 @@
 import { useNavigate } from 'react-router-dom';
 import { PlusMarkSvg } from '../assets/Svg';
 
-const ItemBox = () => {
+const DeviceItemBox = () => {
   const navigate = useNavigate();
 
   return (
     <div>
       <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg'>
         <div className='grid grid-cols-4 gap-4'>
-          {[...Array(1)].map((_, i) => (
+            <div
+              className='flex flex-grow items-center justify-center h-44 rounded-lg bg-gradient-to-r from-gray-400 to-gray-500'
+            >
+              <button
+                className='text-2xl text-gray-400 bg-gray-200 p-2 rounded-lg hover:bg-gray-700 backdrop-blur-3xl drop-shadow-2xl'
+              >
+                <PlusMarkSvg size={`h-4 w-4`} />
+              </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DeviceItemBox;
+
+export const ProjectItemBox = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg'>
+        <div className='grid grid-cols-4 gap-4'>
+          <div className='flex flex-grow items-center justify-center h-44 rounded-lg bg-gradient-to-r from-gray-400 to-gray-500'>
+            <button className='text-2xl text-gray-400 bg-gray-200 p-2 rounded-lg hover:bg-gray-700 backdrop-blur-3xl drop-shadow-2xl'>
+              <PlusMarkSvg size={`h-4 w-4`} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const LabBox = () => {
+  const emojis = ['💄', '🕶️', '👗', '👟'];
+  return (
+    <div>
+      <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg'>
+        <div className='grid grid-cols-4 gap-4'>
+          {emojis.map((e, i) => (
             <div
               key={i}
               className='flex flex-grow items-center justify-center h-44 rounded-lg bg-gradient-to-r from-gray-400 to-gray-500'
             >
               <button
                 onClick={() => navigate(`/app/${i}`)}
-                className='text-2xl text-gray-400 bg-gray-200 p-2 rounded-lg hover:bg-gray-700 backdrop-blur-3xl drop-shadow-2xl'
+                className='text-2xl text-gray-400 bg-gray-200 p-4 rounded-full hover:bg-gray-700 backdrop-blur-3xl drop-shadow-2xl shadow-2xl'
               >
-                <PlusMarkSvg size={`h-4 w-4`} />
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-export default ItemBox;
-
-export const MiniItemBox = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <div className='p-2 border-2 border-gray-200 border-dashed rounded-lg'>
-        <div className='grid grid-cols-3 gap-4'>
-          {[...Array(3)].map((_, i) => (
-            <div
-              key={i}
-              className='flex flex-grow items-center justify-center h-auto w-auto rounded-lg bg-gray-600 hover:bg-gray-700'
-            >
-              <button
-                onClick={() => navigate(`/app/${i}`)}
-                className='text-gray-100 p-2 py-4 rounded-lg backdrop-blur-3xl drop-shadow-2xl'
-              >
-                <PlusMarkSvg size={`h-2 w-2`} />
+                {e}
               </button>
             </div>
           ))}
